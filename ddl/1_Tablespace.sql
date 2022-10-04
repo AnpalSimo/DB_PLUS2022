48f28e20ce18c4b7ead6697272e4bc49c813a011
Simone AnpalServizi <56584723+AnpalSimo@users.noreply.github.com>
2022-10-04 15:29:30 +0200

    Delete ddl directory

--- ddl/1_Tablespace.sql
+++
@@ -1,7 +0,0 @@
-\c :dbName :dbSystemUser;
-\echo 'executing script 1_Tablespace.sql';
-\set tblspcClaDizData :tblspcRoot cladiz/data
-CREATE TABLESPACE cladiz_data OWNER :dbSystemUser LOCATION :'tblspcClaDizData' ;
-
-\set tblspcClaDizIdx :tblspcRoot cladiz/idx
-CREATE TABLESPACE cladiz_idx OWNER :dbSystemUser LOCATION :'tblspcClaDizIdx' ;
